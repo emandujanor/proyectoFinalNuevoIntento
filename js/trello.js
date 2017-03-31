5a96727b241e69f7a1a1a87bdb0a0883e724b0ce
Edith Mandujano <edithmandujano001@gmail.com>
2017-03-31 09:10:17 -0600

    nuevoFinalSprint2NuevoIntento

---
+++ js/trello.js
@@ -0,0 +1,42 @@
+var listaTareas= []; //arreglo donde guardaré la lista de tareas
+var listaPendientes=[];//arreglo donde guardaré las listas pendientes
+var tareas=document.getElementById('tareas');
+var nombreDeLaTarea=document.getElementById("nombreTarea");
+//este es el constructor de la lista de tareas
+function ListaTareas(){
+	var tareas=document.getElementById('tareas');
+	var div=document.tareas.createElement("div");
+	div.id=Date.now();
+	tareas.appendChild("div");
+	//var nombreDeLaTarea=document.tareas.getElementById("nombreTarea");
+	//var divTarjeta=document.createElement("div");
+	
+	}
+console.log(ListaTareas);
+console.log(tareas);
+console.log(div);
+//var NuevaLista=new ListaTareas
+/*
+function CrearBotonNuevoPendiente(){
+		this.boton=document.this.createElement("button");
+		this.boton.innerText="Agregar Nuevo Pendiente";
+		this.boton.setAtribute("onclick", "crearlista();");
+		
+	
+}
+
+function CrearNuevoInput(){
+		this.inputText=document.this.createElement("Input");
+		this.inputText.setAtribute("type", "text");
+		this.inputText.setAtribute("presholder", "Ingrese el nuevo pendiente");
+	
+}
+function crearLista(){
+	
+		this.li=document.createElement("li");
+		tareas.ul.appendChild(li);
+		tareas.ul.li.innerText=
+		
+		
+}*/
+	
